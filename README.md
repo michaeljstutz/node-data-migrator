e3e6e235a0701aec0441ee315e8ededf30507e37
Michael Stutz <michaeljstutz@gmail.com>
2016-06-14 23:57:52 -0400

    Added the majority of the base logic into data-migrator Updated the LICENSE Updated the README, it now includes installation instructions as well as examples on use Updated the test file to include the basic test requirements

--- README.md
+++ README.md
@@ -1,2 +1,96 @@
-# node-data-migrator
-Node.js module for migrating data between two different data objects using paths, normalize, conditions, and hooks
+# DataMigrator
+Node.js module for migrating data between two different data objects using paths, conditions, and normalize functions
+
+## Installation
+Using npm:
+```
+npm install --save data-migrator
+```
+In Node.js
+```
+var DataMigrator = require('data-migrator');
+var dataMigrator = new DataMigrator();
+
+```
+## Quick Examples
+
+```js
+
+var sourceObject = {
+  item1: 1,
+  item2: '2',
+  item3: true,
+  item4: [1,2,3,4],
+  item5: {subItem1: 1, subItem2: '2'},
+};
+
+var targetObject = {
+  newItem1: [],
+  newItem2: {},
+};
+
+dataMigrator.addSource(sourceObject);
+dataMigrator.addTarget(targetObject);
+
+dataMigrator.addPath({from:'item1', to:'newItem2.item1', normalize: function(value){ return value * 10; }});
+dataMigrator.addPath({from:'item2', to:'newItem2.item2', normalize:'number'});
+dataMigrator.addPath({from:'item3', to:'newItem2.item3', fromCondition:'isNumber'});
+dataMigrator.addPath({from:'item4[1]', to:'newItem1[]'}, fromCondition:'isNumber', toCondition:'isArray');
+dataMigrator.addPath({from:'item4[3]', to:'newItem1[]'}, fromCondition:'isNumber', toCondition:'isArray');
+dataMigrator.addPath({from:'item4[0]', to:'newItem1[]'}, fromCondition:'isNumber', toCondition:'isArray');
+dataMigrator.addPath({from:'item4[2]', to:'newItem1[]'}, fromCondition:'isNumber', toCondition:'isArray');
+dataMigrator.addPath({from:'item5.subItem1', to:'newItem2.item5.subItem1', normalize:'string'});
+dataMigrator.addPath({from:'item5.subItem2', to:'newItem2.item5.subItem2', normalize:'string'});
+
+dataMigrator.run(function(err, stats){
+  console.log('dataMigrator.run() has completed with the below stats...');
+  console.log(JSON.stringify(stats, null, 2));
+  console.log(JSON.stringify(targetObject, null, 2));
+});
+```
+###The code should display the following on the console
+```js
+dataMigrator.run() has completed with the below stats...
+stats: {
+  "totalPathsProcessed": 9,
+  "sourceUriNotFound": 0,
+  "fromConditionFailed": 1,
+  "toConditionFailed": 0
+}
+sourceObject: {
+  "item1": 1,
+  "item2": "2",
+  "item3": true,
+  "item4": [
+    1,
+    2,
+    3,
+    4
+  ],
+  "item5": {
+    "subItem1": 1,
+    "subItem2": "2"
+  }
+}
+targetObject: {
+  "newItem1": [
+    2,
+    4,
+    1,
+    3
+  ],
+  "newItem2": {
+    "item1": 10,
+    "item2": 2,
+    "item5": {
+      "subItem1": "1",
+      "subItem2": "2"
+    }
+  }
+}
+
+
+```
+
+Note: the fromConditionFailed: 1 count value is because the condition isNumber failed for item3 (was a boolean)
+
--- test/data-migrator-test.js
+++ test/data-migrator-test.js
@@ -34,10 +34,10 @@ describe('Working with DataMigrator', function(done) {
       ],
     },
   };
-  let testTarget = {};
+  let testTarget = {testing:true};
   let dataMigrator = new DataMigrator();
-  describe('Main functionality', function(done) {
-    it('should be called using new creating a new instance');
+  describe('Base functionality', function(done) {
+    it('should be created using the new keyword to create an instance');
     it('should support passing in starting params');
   });
   describe('setSource() & getSource()', function() {
@@ -71,14 +71,87 @@ describe('Working with DataMigrator', function(done) {
     });
     it('should return the added pathId', function(){
       testParams.from = '';
-      expect(dataMigrator.addPath.bind(dataMigrator, testParams)()).that.is.an('string');
+      expect(dataMigrator.addPath.bind(dataMigrator, testParams)()).that.is.a('string');
+    });
+    it('should throw an error when parms.fromCondition is not valid', function(){
+      testParams.fromCondition = {};
+      expect(dataMigrator.addPath.bind(dataMigrator, testParams)).to.throw('params.fromCondition is not a function or string');
+    });
+    it('should throw an error when parms.fromCondition is not valid', function(){
+      testParams.fromCondition = 'invalid';
+      expect(dataMigrator.addPath.bind(dataMigrator, testParams)).to.throw('params.fromCondition function from string can not be found');
+    });
+    it('should not throw an error with a valid parms.fromCondition string', function(){
+      testParams.fromCondition = 'notNull';
+      expect(dataMigrator.addPath.bind(dataMigrator, testParams)).not.to.throw();
+    });
+    it('should throw an error when parms.toCondition is not valid', function(){
+      testParams.toCondition = {};
+      expect(dataMigrator.addPath.bind(dataMigrator, testParams)).to.throw('params.toCondition is not a function or string');
+    });
+    it('should throw an error when parms.toCondition is not valid', function(){
+      testParams.toCondition = 'invalid';
+      expect(dataMigrator.addPath.bind(dataMigrator, testParams)).to.throw('params.toCondition function from string can not be found');
+    });
+    it('should not throw an error with a valid parms.toCondition string', function(){
+      testParams.toCondition = 'notNull';
+      expect(dataMigrator.addPath.bind(dataMigrator, testParams)).not.to.throw();
+    });
+    it('should support from being a function');
+    it('should support to being a function');
+  });
+  describe('removePath()', function() {
+    it('should remove path given the id passed in');
+  });
+  describe('addCondition()', function() {
+    it('should be able to add a new conditon function');
+  });
+  describe('removeCondition()', function() {
+    it('should be able to remove condition function by key');
+  });
+  describe('addNormalize()', function() {
+    it('should be able to add a new normalize function');
+  });
+  describe('removeNormalize()', function() {
+    it('should be able to remove the normalize function by key');
+  });
+  describe('clearPaths()', function() {
+    it('should clear all the current paths', function(){
+      expect(dataMigrator.clearPaths.bind(dataMigrator)()).that.is.a('boolean', true);
     });
   });
-  describe('run()', function(done) {
-    it('should process all the fields added', function(done){
-      dataMigrator.run({}, (err)=>{
+  describe('run()', function() {
+    it('should supporting passing in most of the params');
+    it('should process all the paths added', function(done){
+      dataMigrator.addPath(
+        {
+          from:'sourceObject1.key1',
+          fromCondition: 'isEqual',
+          fromConditionValue: true,
+        }
+      );
+      dataMigrator.addPath({from:'sourceObject1.key2'});
+      dataMigrator.addPath({from:'sourceObject1.key3'});
+      dataMigrator.addPath({from:'sourceObject1.key4'});
+      dataMigrator.addPath({from:'sourceObject1.key5'});
+      dataMigrator.addPath({from:'sourceObject1.key6'});
+      dataMigrator.run({}, (err, results)=>{
         done();
       });
     });
+    it('should return detailed stats on the results of the function');
+    it('should work with custom from functions');
+    it('should work with custom to functions');
+    it('should work with custom condition functions');
+    it('should work with custom noramlize functions');
+  });
+  describe('addPaths()', function() {
+    it('should support adding in an array of path objects');
+  });
+  describe('exportPaths()', function() {
+    it('should support exporting a list of paths');
+  });
+  describe('importPaths()', function() {
+    it('should support adding in an exported list of paths');
   });
 });
